e2a0b8a9753fdf36c769c7a9c925728fe77e83da
SasoHdz <isaacrdzcmd@gmail.com>
2023-05-22 09:14:05 -0600

    Version base lista

--- src/main/java/com/itq/seguimientorest/service/SeguimientoServiceController.java
+++ src/main/java/com/itq/seguimientorest/service/SeguimientoServiceController.java
@@ -1,7 +1,9 @@
 package com.itq.seguimientorest.service;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itq.seguimientorest.dto.Ack;
@@ -17,4 +19,19 @@ public class SeguimientoServiceController {
         ack.setDescripcion("Creado con exito");
         return (ack);
     }
+    
+    
+
+    @GetMapping("/paquete")
+	public Ack readPaquete(@RequestParam(value =  "id_paquete") Integer id) {
+		Ack ack = new Ack();
+        ack.setCodigo(1);
+        ack.setDescripcion("Paquete"); 
+
+		return ack;
+	}
+
+
+
+
 }
